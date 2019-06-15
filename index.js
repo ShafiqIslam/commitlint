19ec8719ab6a392bf4ff088d3e39ac97e254a66f
Shafiqul Islam <shafiq.xor@gmail.com>
2019-06-15 15:37:15 +0600

    [WIP] move ckecks to commit template projects

--- index.js
+++ index.js
@@ -1,31 +1,20 @@
-const args = process.argv.slice(2);
-const read = require('fs').readFileSync;
+const czrc = require('@sheba/commit-template').czrc;
+const message_string = require('fs').readFileSync(process.argv.slice(2)[1], 'utf-8');
+const message_splits = message_string.replaceAll('\r\n', '\n').trimAny('\n').split('\n\n');
 
-String.prototype.trimAny = function (s) {
-    if (s === " ") return this.trim();
-    if (s === "]") s = "\\]";
-    if (s === "\\") s = "\\\\";
-    let regex = new RegExp("^[" + s + "]+|[" + s + "]+$", "g");
-    return this.replace(regex, "");
-};
-
-String.prototype.replaceAll = function(search, replacement) {
-    var target = this;
-    return target.replace(new RegExp(search, 'g'), replacement);
-};
-
-const message = read(args[1], 'utf-8');
-const message_splits = message.replaceAll('\r\n', '\n').trimAny('\n').split('\n\n');
-if(message_splits.length < 2) {
-    console.log("At least 2 section (subject and types) must be present");
+function exit(message) {
+    console.log(message);
     process.exit(1);
+}
+
+if(message_splits.length < 2) {
+    exit("At least 2 section (subject and types) must be present");
 }
 if(!message_splits[1].startsWith("Type(s):")) {
-    console.log("Type(s) must be present in second section, found: " + message_splits[1]);
-    process.exit(1);
+    exit("Type(s) must be present in second section, found: " + message_splits[1]);
 }
 
-let message_sections = {
+let message = {
     subject: message_splits[0],
     types: message_splits[1].replace('Type(s): ', '').split(', '),
     scopes: [],
@@ -37,22 +26,16 @@ let message_sections = {
 };
 for(let i=2; i<message_splits.length; i++) {
     let split = message_splits[i];
-    if(split.startsWith("Scope(s): ")) message_sections.scopes = split.replace('Scope(s): ', '').split(', ');
-    else if(split.startsWith("Why:\n")) message_sections.why = split.replace('Why:\n', '');
-    else if(split.startsWith("What:\n")) message_sections.what = split.replace('What:\n', '');
-    else if(split.startsWith("Issue(s):\n- ")) message_sections.issues = split.replace('Issue(s):\n- ', '').split('\n- ');
-    else if(split.startsWith("Reference(s):\n- ")) message_sections.references = split.replace('Reference(s):\n- ', '').split('\n- ');
-    else if(split.startsWith("Co Authored By:\n- ")) message_sections.co_authors = split.replace('Co Authored By:\n- ', '').split('\n- ');
+    if(split.startsWith("Scope(s): ")) message.scopes = split.replace('Scope(s): ', '').split(', ');
+    else if(split.startsWith("Why:\n")) message.why = split.replace('Why:\n', '');
+    else if(split.startsWith("What:\n")) message.what = split.replace('What:\n', '');
+    else if(split.startsWith("Issue(s):\n- ")) message.issues = split.replace('Issue(s):\n- ', '').split('\n- ');
+    else if(split.startsWith("Reference(s):\n- ")) message.references = split.replace('Reference(s):\n- ', '').split('\n- ');
+    else if(split.startsWith("Co Authored By:\n- ")) message.co_authors = split.replace('Co Authored By:\n- ', '').split('\n- ');
     else {
-        console.log("Invalid section containing: " + split);
-        process.exit(1);
+        exit("Invalid section containing: " + split);
     }
 }
 
-const CZRC = require('@sheba/commit-template/CZRC.js');
-let czrc = new CZRC();
-czrc.load();
-console.log(czrc);
-//const format = require('./formatter.js')(czrc);
 
-console.log(message_sections);
+console.log(message);
